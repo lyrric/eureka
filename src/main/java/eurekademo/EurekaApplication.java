40801be5097e14a741e759fc65e66c72a281b73e
wangxiaodong <wangxiaodong1@bbdservice.com>
2019-01-29 17:13:09 +0800

    'test'

---
+++ src/main/java/eurekademo/EurekaApplication.java
@@ -0,0 +1,26 @@
+package eurekademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ *
+ * @author Gunnar Hillert
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(EurekaApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+}
